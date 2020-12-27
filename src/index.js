0c3bc77bfcfec3a43f64741d80ec6cf8c112538b
Muniba Waqar <muniba.wq@gmail.com>
2020-12-27 15:02:56 -0700

    basic feed code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   return (
-    <div style={{ fontSize: "10rem", paddingTop: "10rem" }}>Hi there!</div>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="data"> Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
   );
 };
 
