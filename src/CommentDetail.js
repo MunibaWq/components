60c9d13aa76449098cb03f1b1d7667099f9c09ac
Muniba Waqar <muniba.wq@gmail.com>
2020-12-27 15:34:08 -0700

    creating a resuable component

---
+++ src/CommentDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const CommentDetail = () => {
+  return (
+    <div>
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src="https://source.unsplash.com/random" />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span className="data"> Today at 6:00PM</span>
+          </div>
+          <div className="text">Nice blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import CommentDetail from "./CommentDetail";
 
 const App = () => {
   return (
     <div className="ui container comments">
-      <div className="comment">
-        <a href="/" className="avatar">
-          <img alt="avatar" src="https://source.unsplash.com/random" />
-        </a>
-        <div className="content">
-          <a href="/" className="author">
-            Sam
-          </a>
-          <div className="metadata">
-            <span className="data"> Today at 6:00PM</span>
-          </div>
-          <div className="text">Nice blog post!</div>
-        </div>
-      </div>
+      <CommentDetail />
+
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
+      <CommentDetail />
     </div>
   );
 };
