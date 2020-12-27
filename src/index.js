6761654f37027ec5bafd8699263d50a38054ec72
Muniba Waqar <muniba.wq@gmail.com>
2020-12-27 14:56:28 -0700

    styling adding semantic ui

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>Hi there!</div>;
+  return (
+    <div style={{ fontSize: "10rem", paddingTop: "10rem" }}>Hi there!</div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
